1d0bf6a62213afd5372bf9e9b8398dc57ccfc1d7
mikk7440 <miols15@student.sdu.dk>
2018-10-16 17:40:03 +0200

    img 4_1

--- 00_display_image.cpp
+++ 00_display_image.cpp
@@ -17,9 +17,16 @@
 using namespace std;
 using namespace cv;
 
-void show_image(Mat img);
+void show_image(Mat img, string name);
 Mat histogram(const Mat& img);
 Mat draw_histogram(const Mat& hist);
+Mat I2H(Mat img){
+    return draw_histogram(histogram(img));
+}
+void FreqFilt(Mat img, float d0, int n);
+void dftshift(cv::Mat& mag);
+void show_complex(Mat complex, string name);
+Mat butter_lowpass(float d0, int n, cv::Size size);
 
 
 int main(int argc, char* argv[])
@@ -36,78 +43,184 @@ int main(int argc, char* argv[])
           "{@image5 | ./pictures/Image5_optional.png | image path}"
   );
 
-  if (parser.has("help")){
-    parser.printMessage();
-    return 0;
-  }
+    if (parser.has("help")){
+        parser.printMessage();
+        return 0;
+    }
 
-  vector<Mat> images;
+    // Load image as grayscale
+    string filepath = parser.get<string>("@image1");
+    Mat img1 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
+    Mat img1Edit = img1;
 
-  // Load image as grayscale
-  string filepath = parser.get<string>("@image1");
-  Mat img1 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
-  Mat img1Edit = img1;
+    filepath = parser.get<string>("@image2");
+    Mat img2 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
 
-  filepath = parser.get<string>("@image2");
-  Mat img2 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
+    filepath = parser.get<string>("@image3");
+    Mat img3 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
 
-  filepath = parser.get<string>("@image3");
-  Mat img3 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
+    filepath = parser.get<string>("@image4_1");
+    Mat img4_1 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
 
-  filepath = parser.get<string>("@image4_1");
-  Mat img4_1 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
+    filepath = parser.get<string>("@image4_2");
+    Mat img4_2 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
 
-  filepath = parser.get<string>("@image4_2");
-  Mat img4_2 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
+    filepath = parser.get<string>("@image5");
+    Mat img5 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
 
-  filepath = parser.get<string>("@image5");
-  Mat img5 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
+    filepath = parser.get<string>("@imageO");
+    Mat img0 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
 
-  filepath = parser.get<string>("@imageO");
-  Mat img0 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
+    if(img1.empty()){
+        cout << "No image found" << endl;
+        return -1;
+    }
 
-  if(img1.empty()){
-    cout << "No image found" << endl;
-    return -1;
-  }
+    // Original img.
+    show_image(img0, "Original Don't use");
 
+/***************************************************************************
+*                                                                          *
+*                           Image 4_1                                      *
+*                                                                          *
+***************************************************************************/
 
-  namedWindow("og", WINDOW_NORMAL);
-  imshow ("og", img0);
+    show_image(img4_1, "img4_1 org");
+    show_image(I2H(img4_1), "histogram org");
 
-  namedWindow("Org", WINDOW_NORMAL);
-  imshow ("Org", img2);
+    Mat img = img4_1;
 
+    //Mat lowPassF = butter_lowpass(250, 5, img.size());
 
+    FreqFilt(img, 250, 100);
 
+    Mat hist = histogram(img1);
+    //  imshow("Histgram Orgi", draw_histogram(hist));
+    // imshow("Histgram Filtered", draw_histogram(hist));
 
-  Mat hist = histogram(img1);
-//  imshow("Histgram Orgi", draw_histogram(hist));
-  hist = histogram(img1Filt);
-//  imshow("Histgram Filtered", draw_histogram(hist));
-
-  Mat eqimg;
-  equalizeHist(img1, eqimg);
-  //imshow("Equalized image", eqimg);
-
-  Mat eqhist = histogram(eqimg);
-  //imshow("Equlized histogram", eqhist);
+    Mat eqimg;
+    equalizeHist(img1, eqimg);
 
 
-  // Printing the images
-  //show_image(img);
-
+/***************************************************************************
+*                                                                          *
+*                           Exit routeine                                  *
+*                                                                          *
+***************************************************************************/
   while (cv::waitKey() != 27) // Wait for esc key
       ; // (do nothing)
-
 }
 
 
+//KIM
+void FreqFilt(Mat img, float d0, int n){
+    cv::Mat padded;
+        int opt_rows = cv::getOptimalDFTSize(img.rows * 2 - 1);
+        int opt_cols = cv::getOptimalDFTSize(img.cols * 2 - 1);
+        cv::copyMakeBorder(img, padded, 0, opt_rows - img.rows, 0, opt_cols - img.cols,
+                           cv::BORDER_CONSTANT, cv::Scalar::all(0));
+
+        cv::Mat planes[] = {
+            cv::Mat_<float>(padded),
+            cv::Mat_<float>::zeros(padded.size())
+        };
+        cv::Mat complex;
+        cv::merge(planes, 2, complex);
+
+        // Compute DFT of image
+        cv::dft(complex, complex);
+//        show_complex(complex, "origin");
+
+        // Shift quadrants to center
+        dftshift(complex);
+
+        Mat filter = butter_lowpass(d0, n, complex.size());
+
+        // Multiply Fourier image with filter
+        cv::mulSpectrums(complex, filter, complex, 0);
+
+        // Shift back
+        dftshift(complex);
+//        show_complex(complex, "filtered");
+        // Compute inverse DFT
+        cv::Mat filtered;
+        cv::idft(complex, filtered, (cv::DFT_SCALE | cv::DFT_REAL_OUTPUT));
+
+        // Crop image (remove padded borders)
+        filtered = cv::Mat(filtered, cv::Rect(cv::Point(0, 0), img.size()));
+
+        dftshift(filter);
+//        show_complex(filter, "Filter");
+
+        cv::normalize(filtered, filtered, 0, 1, cv::NORM_MINMAX);
+        show_image(filtered, "filtered image");
+}
+// KIM
+cv::Mat butter_lowpass(float d0, int n, cv::Size size)
+{
+    cv::Mat_<cv::Vec2f> lpf(size);
+    cv::Point2f c = cv::Point2f(size) / 2;
+
+    for (int i = 0; i < size.height; ++i) {
+        for (int j = 0; j < size.width; ++j) {
+            // Distance from point (i,j) to the origin of the Fourier transform
+            float d = std::sqrt((i - c.y) * (i - c.y) + (j - c.x) * (j - c.x));
+
+            // Real part
+            lpf(i, j)[0] = 1 / (1 + std::pow(d / d0, 2 * n));
+
+            // Imaginary part
+            lpf(i, j)[1] = 0;
+        }
+    }
+
+    return lpf;
+}
+// https://vgg.fiit.stuba.sk/2012-05/frequency-domain-filtration/
+// KIM // Rearranges the quadrants of a Fourier image so that the origin is at the
+// center of the image.
+void dftshift(cv::Mat& mag)
+{
+    int cx = mag.cols / 2;
+    int cy = mag.rows / 2;
+
+    cv::Mat tmp;
+    cv::Mat q0(mag, cv::Rect(0, 0, cx, cy));
+    cv::Mat q1(mag, cv::Rect(cx, 0, cx, cy));
+    cv::Mat q2(mag, cv::Rect(0, cy, cx, cy));
+    cv::Mat q3(mag, cv::Rect(cx, cy, cx, cy));
+
+    q0.copyTo(tmp);
+    q3.copyTo(q0);
+    tmp.copyTo(q3);
+
+    q1.copyTo(tmp);
+    q2.copyTo(q1);
+    tmp.copyTo(q2);
+}
+
+void show_complex(Mat complex, string name){
+    Mat magI;
+    Mat planes[] = {
+        Mat::zeros(complex.size(), CV_32F),
+        Mat::zeros(complex.size(), CV_32F)
+    };
+    split(complex, planes); // planes[0] = Re(DFT(I)), planes[1] = Im(DFT(I))
+    magnitude(planes[0], planes[1], magI); // sqrt(Re(DFT(I))^2 + Im(DFT(I))^2)
+    // switch to logarithmic scale: log(1 + magnitude)
+    magI += Scalar::all(1);
+    log(magI, magI);
+    dftshift(magI); // rearrage quadrants
+    // Transform the magnitude matrix into a viewable image (float values 0-1)
+    normalize(magI, magI, 1, 0, NORM_INF);
+    namedWindow(name, WINDOW_NORMAL);
+    imshow(name, magI);
+}
 
 
-void show_image(Mat img){
-  namedWindow("Display window", WINDOW_NORMAL);
-  imshow("Display window", img);
+void show_image(Mat img, string name){
+  namedWindow(name, WINDOW_NORMAL);
+  imshow(name, img);
 }
 
 Mat histogram(const Mat& img){
@@ -127,7 +240,6 @@ Mat histogram(const Mat& img){
 
   return histogram;
 }
-
 Mat draw_histogram(const Mat& hist){
   int nbins = hist.rows;
   double max = 0;
@@ -142,7 +254,6 @@ Mat draw_histogram(const Mat& hist){
 
   return img;
 }
-
 
 /*
   // Filter inspired by https://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/filter_2d/filter_2d.html
