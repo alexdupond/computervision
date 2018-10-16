be81b7e3d6d23a9d7ad35caf2af22c4fa985aad5
mikk7440 <miols15@student.sdu.dk>
2018-10-16 15:26:15 +0200

    small

--- 00_display_image.cpp
+++ 00_display_image.cpp
@@ -11,6 +11,8 @@
 
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
+#include <stdio.h>
 
 using namespace std;
 using namespace cv;
@@ -19,12 +21,19 @@ void show_image(Mat img);
 Mat histogram(const Mat& img);
 Mat draw_histogram(const Mat& hist);
 
+
 int main(int argc, char* argv[])
 {
 
   CommandLineParser parser(argc, argv,
           "{help   |            | print this message}"
-          "{@image | ./lena.bmp | image path}"
+          "{@imageO | ./pictures/ImageOrig.png | image path}"
+          "{@image1 | ./pictures/Image1.png | image path}"
+          "{@image2 | ./pictures/Image2.png | image path}"
+          "{@image3 | ./pictures/Image3.png | image path}"
+          "{@image4_1 | ./pictures/Image4_1.png | image path}"
+          "{@image4_2 | ./pictures/Image4_2.png | image path}"
+          "{@image5 | ./pictures/Image5_optional.png | image path}"
   );
 
   if (parser.has("help")){
@@ -32,33 +41,66 @@ int main(int argc, char* argv[])
     return 0;
   }
 
-      // Load image as grayscale
-  string filepath = parser.get<string>("@image");
-  Mat img = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
+  vector<Mat> images;
 
-  if(img.empty()){
+  // Load image as grayscale
+  string filepath = parser.get<string>("@image1");
+  Mat img1 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
+  Mat img1Edit = img1;
+
+  filepath = parser.get<string>("@image2");
+  Mat img2 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
+
+  filepath = parser.get<string>("@image3");
+  Mat img3 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
+
+  filepath = parser.get<string>("@image4_1");
+  Mat img4_1 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
+
+  filepath = parser.get<string>("@image4_2");
+  Mat img4_2 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
+
+  filepath = parser.get<string>("@image5");
+  Mat img5 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
+
+  filepath = parser.get<string>("@imageO");
+  Mat img0 = imread(filepath, IMREAD_GRAYSCALE); // Loads as grayscale
+
+  if(img1.empty()){
     cout << "No image found" << endl;
     return -1;
   }
 
-  Mat hist = histogram(img);
-  imshow("Histgram", draw_histogram(hist));
+
+  namedWindow("og", WINDOW_NORMAL);
+  imshow ("og", img0);
+
+  namedWindow("Org", WINDOW_NORMAL);
+  imshow ("Org", img2);
+
+
+
+
+  Mat hist = histogram(img1);
+//  imshow("Histgram Orgi", draw_histogram(hist));
+  hist = histogram(img1Filt);
+//  imshow("Histgram Filtered", draw_histogram(hist));
 
   Mat eqimg;
-  equalizeHist(img, eqimg);
+  equalizeHist(img1, eqimg);
   //imshow("Equalized image", eqimg);
 
   Mat eqhist = histogram(eqimg);
   //imshow("Equlized histogram", eqhist);
 
+
   // Printing the images
   //show_image(img);
 
-  waitKey(0);
-  return 0;
+  while (cv::waitKey() != 27) // Wait for esc key
+      ; // (do nothing)
+
 }
-
-
 
 
 
@@ -82,7 +124,7 @@ Mat histogram(const Mat& img){
     {256}, // Hisogram size / Number of bins
     {0, 256} // Pairs of bin lower and upper
   );
-  histogram.at<float>(0) = 0;
+
   return histogram;
 }
 
@@ -100,3 +142,38 @@ Mat draw_histogram(const Mat& hist){
 
   return img;
 }
+
+
+/*
+  // Filter inspired by https://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/filter_2d/filter_2d.html
+  int kernel_size=30;
+  Point anchor = Point( -1, -1 );
+  double delta = 0;
+  int ddepth =-1;
+  Mat img1Filt;
+  img1Edit=img2;
+
+  Mat kernel = cv::Mat::ones(kernel_size,kernel_size, CV_32F)/ (float)(kernel_size*kernel_size);
+//  cv::filter2D(img1Edit, img1Filt, ddepth , kernel, anchor, delta, BORDER_DEFAULT );
+  cv::bilateralFilter(img1Edit, img1Filt, 9, 50, 50);
+
+  namedWindow("Linear filter", WINDOW_NORMAL);
+  imshow("Linear filter",img1Filt);
+
+  namedWindow("hist", WINDOW_NORMAL);
+  imshow("hist",draw_histogram(histogram(img1Filt)));
+
+  Mat frame;
+  cv::GaussianBlur( img1Filt,frame, cvSize(0, 0), 3);
+
+  namedWindow("frame", WINDOW_NORMAL);
+  imshow("frame",frame);
+
+  cv::addWeighted(frame, 5 , img1Filt, -0.5, 0, img1Filt);
+
+
+  namedWindow("Filtered", WINDOW_NORMAL);
+  imshow ("Filtered",img1Filt);
+
+
+*/
