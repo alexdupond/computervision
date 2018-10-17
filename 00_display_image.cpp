7673e6db3a21f490feffaaa78536be78d2978a7b
mikk7440 <miols15@student.sdu.dk>
2018-10-17 16:03:42 +0200

    "4.1"

--- 00_display_image.cpp
+++ 00_display_image.cpp
@@ -27,7 +27,9 @@ void FreqFilt(Mat img, float d0, int n);
 void dftshift(cv::Mat& mag);
 void show_complex(Mat complex, string name);
 Mat butter_lowpass(float d0, int n, cv::Size size);
-
+Mat notch_filter(float area, int freksX , int freksY, Size size);
+void FreqFiltNotch(Mat img, float area, int freksX,int freksY);
+void pic3(Mat img);
 
 int main(int argc, char* argv[])
 {
@@ -77,30 +79,40 @@ int main(int argc, char* argv[])
     }
 
     // Original img.
-    show_image(img0, "Original Don't use");
+//    show_image(img0, "Original Don't use");
 
 /***************************************************************************
 *                                                                          *
 *                           Image 4_1                                      *
 *                                                                          *
 ***************************************************************************/
-
+/*
     show_image(img4_1, "img4_1 org");
     show_image(I2H(img4_1), "histogram org");
 
     Mat img = img4_1;
 
-    //Mat lowPassF = butter_lowpass(250, 5, img.size());
+    Rect ROI(700,1400,800,400);
+    img4_1 = img4_1(ROI);
+    show_image(img4_1,"ROI Org");
+*/
+    //FreqFilt(img, 250, 100); // Diameter, n
+   // FreqFiltNotch(img,50,2140,1760);
 
-    FreqFilt(img, 250, 100);
 
-    Mat hist = histogram(img1);
+//    Mat hist = histogram(img1);
     //  imshow("Histgram Orgi", draw_histogram(hist));
     // imshow("Histgram Filtered", draw_histogram(hist));
 
-    Mat eqimg;
-    equalizeHist(img1, eqimg);
+//    Mat eqimg;
+//    equalizeHist(img1, eqimg);
 
+// IMG 3
+   show_image(img3, "img3");
+   namedWindow("hist org", WINDOW_NORMAL);
+   imshow("hist org",draw_histogram(histogram(img3)));
+
+    pic3(img3);
 
 /***************************************************************************
 *                                                                          *
@@ -110,7 +122,71 @@ int main(int argc, char* argv[])
   while (cv::waitKey() != 27) // Wait for esc key
       ; // (do nothing)
 }
+void FreqFiltNotch(Mat img, float area, int freksX,int freksY){
+    cv::Mat padded;
+        int opt_rows = cv::getOptimalDFTSize(img.rows * 2 - 1);
+        int opt_cols = cv::getOptimalDFTSize(img.cols * 2 - 1);
+        cv::copyMakeBorder(img, padded, 0, opt_rows - img.rows, 0, opt_cols - img.cols,
+                           cv::BORDER_CONSTANT, cv::Scalar::all(0));
 
+        cv::Mat planes[] = {
+            cv::Mat_<float>(padded),
+            cv::Mat_<float>::zeros(padded.size())
+        };
+        cv::Mat complex;
+        cv::merge(planes, 2, complex);
+
+        // Compute DFT of image
+        cv::dft(complex, complex);
+      show_complex(complex, "origin");
+
+        // Shift quadrants to center
+        dftshift(complex);
+
+        //Øverst højre hjørne
+        Mat filter = notch_filter( area, freksX, freksY , complex.size());
+        // Multiply Fourier image with filter
+        cv::mulSpectrums(complex, filter, complex, 0);
+
+        //Øverste venstre hjørne
+        filter = notch_filter( area, 1350, 2200, complex.size());
+        cout << "freksX X: " << freksX+1324 << " | Y: " << freksY-1175 << endl;
+        // Multiply Fourier image with filter
+        cv::mulSpectrums(complex, filter, complex, 0);
+
+        //Neders højre hjørne
+        filter = notch_filter( area, 1740 , 2640, complex.size());
+        cout << "freksX X: " << freksY-350 << " | Y: " << freksX+924 << endl;
+        // Multiply Fourier image with filter
+        cv::mulSpectrums(complex, filter, complex, 0);
+
+        filter = notch_filter( area, 925, 3064, complex.size());
+        cout << "freksX X: " << freksY-700 << " | Y: " << freksX+400 << endl;
+        // Multiply Fourier image with filter
+        cv::mulSpectrums(complex, filter, complex, 0);
+
+        // Shift back
+        dftshift(complex);
+        show_complex(complex, "filtered");
+        // Compute inverse DFT
+        cv::Mat filtered;
+        cv::idft(complex, filtered, (cv::DFT_SCALE | cv::DFT_REAL_OUTPUT));
+
+        // Crop image (remove padded borders)
+        filtered = cv::Mat(filtered, cv::Rect(cv::Point(0, 0), img.size()));
+
+        dftshift(filter);
+//        show_complex(filter, "Filter");
+
+        cv::normalize(filtered, filtered, 0, 1, cv::NORM_MINMAX);
+        show_image(filtered, "filtered image");
+
+        Rect ROI(700,1400,800,400);
+        filtered = filtered(ROI);
+        show_image(filtered,"ROI");
+
+        //I2H(filtered);
+}
 
 //KIM
 void FreqFilt(Mat img, float d0, int n){
@@ -129,7 +205,7 @@ void FreqFilt(Mat img, float d0, int n){
 
         // Compute DFT of image
         cv::dft(complex, complex);
-//        show_complex(complex, "origin");
+      show_complex(complex, "origin");
 
         // Shift quadrants to center
         dftshift(complex);
@@ -141,7 +217,7 @@ void FreqFilt(Mat img, float d0, int n){
 
         // Shift back
         dftshift(complex);
-//        show_complex(complex, "filtered");
+        show_complex(complex, "filtered");
         // Compute inverse DFT
         cv::Mat filtered;
         cv::idft(complex, filtered, (cv::DFT_SCALE | cv::DFT_REAL_OUTPUT));
@@ -154,17 +230,18 @@ void FreqFilt(Mat img, float d0, int n){
 
         cv::normalize(filtered, filtered, 0, 1, cv::NORM_MINMAX);
         show_image(filtered, "filtered image");
+        //I2H(filtered);
 }
 // KIM
-cv::Mat butter_lowpass(float d0, int n, cv::Size size)
+Mat butter_lowpass(float d0, int n, Size size)
 {
-    cv::Mat_<cv::Vec2f> lpf(size);
-    cv::Point2f c = cv::Point2f(size) / 2;
+    Mat_<Vec2f> lpf(size);
+    Point2f c = cv::Point2f(size) / 2;
 
     for (int i = 0; i < size.height; ++i) {
         for (int j = 0; j < size.width; ++j) {
             // Distance from point (i,j) to the origin of the Fourier transform
-            float d = std::sqrt((i - c.y) * (i - c.y) + (j - c.x) * (j - c.x));
+            float d = sqrt((i - c.y) * (i - c.y) + (j - c.x) * (j - c.x));
 
             // Real part
             lpf(i, j)[0] = 1 / (1 + std::pow(d / d0, 2 * n));
@@ -176,6 +253,27 @@ cv::Mat butter_lowpass(float d0, int n, cv::Size size)
 
     return lpf;
 }
+cv::Mat notch_filter(float area, int freksX , int freksY, Size size){
+    Mat_<Vec2f> nFilter(size);
+
+    for (int i = 0; i < size.height; ++i) {
+        for (int j = 0; j < size.width; ++j) {
+             nFilter(i, j)[0] =1;
+            if( i>freksY-area && i<freksY+area)
+            {
+                if (j>freksX-area && j<freksX+area)
+                {
+                    nFilter(i, j)[0] =0;
+                   // cout << "i,j: " << i << ", " << j <<  endl;
+                }
+            }
+            nFilter(i, j)[1] = 0;
+        }
+    }
+    return nFilter;
+}
+
+
 // https://vgg.fiit.stuba.sk/2012-05/frequency-domain-filtration/
 // KIM // Rearranges the quadrants of a Fourier image so that the origin is at the
 // center of the image.
@@ -253,6 +351,40 @@ Mat draw_histogram(const Mat& hist){
   }
 
   return img;
+}
+
+void pic3(Mat img)
+{
+    // Filter inspired by https://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/filter_2d/filter_2d.html
+    int kernel_size=30;
+    Point anchor = Point( -1, -1 );
+    double delta = 0;
+    int ddepth =-1;
+    Mat img1Filt;
+    Mat img1Edit;
+    img1Edit=img;
+
+
+    Mat kernel = cv::Mat::ones(kernel_size,kernel_size, CV_32F)/ (float)(kernel_size*kernel_size);
+  //  cv::filter2D(img1Edit, img1Filt, ddepth , kernel, anchor, delta, BORDER_DEFAULT );
+    cv::bilateralFilter(img1Edit, img1Filt, 5, 50, 50);
+
+//    namedWindow("Linear filter", WINDOW_NORMAL);
+//    imshow("Linear filter",img1Filt);
+
+    Mat frame;
+    cv::GaussianBlur( img1Filt,frame, cvSize(0, 0), 3);
+
+//    namedWindow("frame", WINDOW_NORMAL);
+//    imshow("frame",frame);
+
+    cv::addWeighted(img1Filt, 1.4 ,frame , -0.5, 0, img1Filt);
+
+    namedWindow("Filtered", WINDOW_NORMAL);
+    imshow ("Filtered",img1Filt);
+
+    namedWindow("hist filtered", WINDOW_NORMAL);
+    imshow("hist filtered",draw_histogram(histogram(img1Filt)));
 }
 
 /*
